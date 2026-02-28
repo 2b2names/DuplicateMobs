f04b3aceb7f1dfffd860df3e961cffa2fc241aac
B56490 <muhammedhalibek@icloud.com>
2026-02-28 18:18:51 +0300

    Initial Commit

---
+++ src/main/java/me/tiger/duplicateMobs/DuplicateMobs.java
@@ -0,0 +1,17 @@
+package me.tiger.duplicateMobs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DuplicateMobs extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
