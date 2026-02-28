8a4d367a563f882da5af0e53c444e88d13e2d499
B56490 <muhammedhalibek@icloud.com>
2026-02-28 18:37:25 +0300

    Update DuplicateMobs.java

--- src/main/java/me/tiger/duplicateMobs/DuplicateMobs.java
+++ src/main/java/me/tiger/duplicateMobs/DuplicateMobs.java
@@ -2,16 +2,26 @@ package me.tiger.duplicateMobs;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class DuplicateMobs extends JavaPlugin {
+public class DuplicateMobs extends JavaPlugin {
+
+    public static final int DUPLICATE_CAP = 50; // your cap
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        DuplicateManager manager = new DuplicateManager(this);
 
+        getServer().getPluginManager().registerEvents(new DuplicateListener(manager), this);
+
+        var cmd = getCommand("duplicatemobs");
+        if (cmd != null) {
+            cmd.setExecutor(new DuplicateMobsCommand(manager));
+        }
+
+        getLogger().info("DuplicateMobs enabled.");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("DuplicateMobs disabled.");
     }
 }
